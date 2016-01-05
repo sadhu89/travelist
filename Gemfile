7827a04e05654d7753d302c5218b484298c5dfbe
Carlos Rojas <carlos.sadhu@gmail.com>
2016-01-04 22:50:13 -0500

    Add destination position a special CSS tag for the first destination

--- Gemfile
+++ Gemfile
@@ -23,6 +23,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'mongoid', '~>5.0.0'
 gem 'geocoder'
+gem 'mongoid_orderable'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,13 +1,17 @@
 class Destination
   include Mongoid::Document
   include Geocoder::Model::Mongoid
+  include Mongoid::Orderable
+
   field :name, type: String
   field :description, type: String
   field :priority, type: Integer
   field :image_url, type: String
   field :coordinates, type: Array
   field :location, type: Array
-  geocoded_by :name               # can also be an IP address
-  after_validation :geocode          # auto-fetch coordinates
+  
+  geocoded_by :name                   # can also be an IP address
+  after_validation :geocode           # auto-fetch coordinates
+  orderable base: 1                   # create postion field
 
 end
