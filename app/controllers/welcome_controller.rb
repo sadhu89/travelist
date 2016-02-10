a99b194febb016d057cbabc40d68799bbe5bbaf4
Carlos Rojas <carlos.sadhu@gmail.com>
2016-02-09 23:37:02 -0500

    Add redirection from landing when logged in

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
+  before_filter :auth_user
+
+  def auth_user
+    redirect_to destinations_url if user_signed_in?
+  end
+
   def index
   end
 end
